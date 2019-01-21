b4f02203943a72284e3a0c5e98289cfce82e1b5b
Christoph Gruener <christophgruener@me.com>
2019-01-21 21:59:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def great(name)
-  puts "hello #{name}, you're great!"
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
 
-def great_height(name, height)
-  puts "hello, #{name}, you're a tall person at #{height} inches tall!"
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
